c67db49c797d7be5d2078f05ba25afb227f17de5
UFOVERSE <ufoverse@outlook.com>
2023-02-20 23:25:05 +0800

    feat: Draw UI and init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import avatar from "./images/avatar.svg";
+import logo from "./images/logo.svg";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import styles from "./App.module.css";
 
 function App() {
+  const recipientName = "Recipient Name";
+  const recipientAddress = "0xF7129631f...e341e2161C4";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <div className={styles.AppHeaderRecipientInfo}>
+          <div>Donate to {recipientName}</div>
+          <div>To:{recipientAddress}</div>
+        </div>
+        <div>
+          <img src={avatar} className="App-avatar" alt="avatar" />
+        </div>
       </header>
+      <section className={styles.AppContent}>
+        <div className={styles.Title}>Payment Method</div>
+        <div className={styles.MethodInput}>
+          <div>images</div>
+          <div>ETH</div>
+          <input placeholder="Ethereum"></input>
+          <div>切换</div>
+        </div>
+        <div className={styles.FooterMark}>
+          <div>icon</div>
+          <div>21.11ETH</div>
+          <div>0.01E = $127; 31GWEI = $0.75</div>
+        </div>
+        <div className={styles.ShortcutOption}>
+          <div>0.001 ETH</div>
+          <div>0.001 ETH</div>
+          <div>0.001 ETH</div>
+        </div>
+        <fieldset>
+          <legend>
+            <span>OR</span>
+          </legend>
+        </fieldset>
+        <input
+          className={styles.PriceBtn}
+          placeholder="Enter Price Manually"
+        ></input>
+        <div className={styles.Msg}>
+          <div>Message</div>
+          <input placeholder="Will be published on chain" multiple></input>
+        </div>
+        <div className={styles.Donate3Btn}>
+          <div>DONATE3</div>
+          <div>≈$875.32</div>
+          <ConnectButton />
+        </div>
+      </section>
+      <footer className={styles.AppFooter}>
+        <span>Power by</span>
+        <span>
+          <img src={logo} alt="logo"></img>
+        </span>
+        <span>Donate3</span>
+      </footer>
     </div>
   );
 }
+
+//
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./polyfills";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import App from "./App";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.REACT_APP_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit demo",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
