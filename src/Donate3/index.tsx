1195d709697d69424e599c9ec3f93661e5b06d8f
0xhardman <1142783160@qq.com>
2023-10-30 00:17:42 +0800

    fix joyid prd

--- src/Donate3/index.tsx
+++ src/Donate3/index.tsx
@@ -56,6 +56,7 @@ const wagmiConfig = createConfig({
             options: {
               name: 'Donate3',
               logo: 'https://fav.farm/🆔',
+              joyidAppURL: 'https://app.joy.id',
             },
           }),
         ],
