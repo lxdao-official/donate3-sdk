407a680406bb079d466ea9f13efe8674e8b861ea
WGB5445 <919603023@qq.com>
2023-12-09 22:17:59 +0800

    Fix widget.js data attribute name

--- src/Donate3/Donate3.stories.tsx
+++ src/Donate3/Donate3.stories.tsx
@@ -1,24 +1,37 @@
-import Donate3 from './index';
 import React from 'react';
+import Donate3 from './index';
 export default {
-    title: 'Donate3',
+  title: 'Donate3',
 };
 const config = {
-    type: 'embed', // 0 Float mode，1 Normal mode float\embed
-    cid: 'bafkreibnfk3tnrmqpgn2b3ynqo7lp7wcolrynuspq54o2dwp25dshmmmou',
-    color: "#666",
-    title: "Donate3",
-    accountType: 1, // 账户类型 0： EOA， 1：safe account
-    toAddress: "0xe395B9bA2F93236489ac953146485C435D1A267B",
-    // avatar:'https://nftstorage.link/ipfs/bafkreidovf46msp6yqpsbfbl2n6whvdyfsupwwpucdguvkgt2isdnbac2i',
-    avatar: '',
-    safeAccounts: [{
-        networkId: 1, address: "0xe395B9bA2F93236489ac953146485C435D1A267B"
-    }, { networkId: 10, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 42161, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 137, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 59144, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 5, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 80001, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" },
-    { networkId: 11155111, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }, { networkId: 420, address: "0xe395B9bA2F93236489ac953146485C435D1A267B" }],
-    // safeAccounts: [{networkId: 5, address: '0xe395B9bA2F93236489ac953146485C435D1A267B'}], // [{networkId: 5, address: '0xd2567eb0893c8b5de7deac1cb66d8d60178767e8'}]
-    demo: false
-}
+  type: 'embed', // 0 Float mode，1 Normal mode float\embed
+  cid: 'bafkreibnfk3tnrmqpgn2b3ynqo7lp7wcolrynuspq54o2dwp25dshmmmou',
+  color: '#666',
+  title: 'Donate3',
+  accountType: 1, // 账户类型 0： EOA， 1：safe account
+  toAddress: '0xe395B9bA2F93236489ac953146485C435D1A267B',
+  // avatar:'https://nftstorage.link/ipfs/bafkreidovf46msp6yqpsbfbl2n6whvdyfsupwwpucdguvkgt2isdnbac2i',
+  avatar: '',
+  safeAccounts: [
+    {
+      networkId: 1,
+      address: '0xe395B9bA2F93236489ac953146485C435D1A267B',
+    },
+    { networkId: 10, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    { networkId: 42161, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    { networkId: 137, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    { networkId: 59144, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    { networkId: 5, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    { networkId: 80001, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+    {
+      networkId: 11155111,
+      address: '0xe395B9bA2F93236489ac953146485C435D1A267B',
+    },
+    { networkId: 420, address: '0xe395B9bA2F93236489ac953146485C435D1A267B' },
+  ],
+  // safeAccounts: [{networkId: 5, address: '0xe395B9bA2F93236489ac953146485C435D1A267B'}], // [{networkId: 5, address: '0xd2567eb0893c8b5de7deac1cb66d8d60178767e8'}]
+  demo: false,
+};
 export const Donate3Primary = () => {
-    return <Donate3 config={config} />
-} 
+  return <Donate3 config={config} />;
+};
--- widget/widget.js
+++ widget/widget.js
@@ -6,7 +6,7 @@ import { getFasterIpfsLink } from '../src/Donate3/utils/ipfsTools';
 // dom selection priority, cid is preferred, followed by address
 // cid > address
 const cidDom = document.querySelectorAll('[data-donate3-cid]');
-const addressDom = document.querySelectorAll('[data-donate3-to-address]');
+const addressDom = document.querySelectorAll('[data-donate3-address]');
 
 const ipfsFlag = cidDom?.length > 0 && cidDom?.[0]?.dataset.donate3Cid;
 const donate3Roots = ipfsFlag ? cidDom : addressDom;
@@ -43,28 +43,53 @@ const getInfoFromIpfs = async (root) => {
     console.error('error', 'getFasterIpfsLink-error');
   }
 };
-
+const getInfoFromBackend = async (root) => {
+  const {
+    type = 0,
+    color,
+    name,
+    address,
+    avatar,
+  } = (
+    await (
+      await fetch(
+        `https://donate3.0xhardman.xyz/settings/${root.getAttribute(
+          'data-donate3-address',
+        )}`,
+      )
+    ).json()
+  ).data;
+  return {
+    type,
+    color,
+    title: name,
+    toAddress: address,
+    avatar,
+    demo: false,
+  };
+};
 const getDonate3Params = (root, isIpfs) => {
   if (isIpfs) {
     return getInfoFromIpfs(root);
   } else {
-    return {
-      type: root.dataset.donate3Type,
-      color: root.dataset.donate3Color,
-      title: root.dataset.donate3Title,
-      toAddress: root.dataset.donate3ToAddress,
-      demo: root.dataset.donate3Demo === 'true' ? true : false,
-      avatar: root.dataset.donate3Avatar,
-      accountType: root.dataset.accountType,
-      safeAccounts:
-        root.dataset.donate3Safeaccounts &&
-        JSON.parse(root.dataset.donate3Safeaccounts),
-    };
+    return getInfoFromBackend(root);
+    // return {
+    //   type: root.dataset.donate3Type,
+    //   color: root.dataset.donate3Color,
+    //   title: root.dataset.donate3Title,
+    //   toAddress: root.dataset.donate3ToAddress,
+    //   demo: root.dataset.donate3Demo === 'true' ? true : false,
+    //   avatar: root.dataset.donate3Avatar,
+    //   accountType: root.dataset.accountType,
+    //   safeAccounts:
+    //     root.dataset.donate3Safeaccounts &&
+    //     JSON.parse(root.dataset.donate3Safeaccounts),
+    // };
   }
 };
 
 donate3Roots.forEach(async (root) => {
-  const config = await getDonate3Params(root, ipfsFlag);
+  const config = await getInfoFromBackend(root);
   const reactRoot = ReactDOM.createRoot(root);
 
   reactRoot.render(<Donate3 config={config} />);
